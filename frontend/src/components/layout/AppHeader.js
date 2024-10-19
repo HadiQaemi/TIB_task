8a24cd4c751a6ef182899bb6ead1630af098f492
Hadi Ghaemi <hadi.qaemi@gmail.com>
2024-10-19 17:31:58 +0200

    Add logo

--- frontend/src/components/layout/AppHeader.js
+++ frontend/src/components/layout/AppHeader.js
@@ -1,51 +1,35 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Form, InputGroup, Button, Container } from 'react-bootstrap';
-import { FaSearch, FaPlus, FaUser } from 'react-icons/fa';
+import { Navbar, Nav, Form, InputGroup, Container } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
+import OrkgLogo from '../../assets/img/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function AppHeader() {
   return (
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Logo</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown title="View" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Tools" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="About" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form className="d-flex flex-column flex-lg-row gap-2">
-              <InputGroup>
-                <Form.Control
-                  placeholder="Search"
-                  aria-label="Search"
-                  aria-describedby="basic-addon1"
-                />
-                <InputGroup.Text id="basic-addon1"><FaSearch /></InputGroup.Text>
-              </InputGroup>
-              <Button type="button" className='button-red'><FaPlus /> Add new</Button>
-              <Button type="button" variant="outline-secondary"><FaUser /> Sign in</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
-  }
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/" className='relative'>
+          <img src={OrkgLogo} alt="ORKG Logo" width="100" />
+          <div className='title absolute block'>Reborn</div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/paper/all" className='header-link'>Paper view</Link>
+            <Link to="/statement/all" className='header-link'>Statement view</Link>
+          </Nav>
+          <Form className="d-flex flex-column flex-lg-row gap-2 w-50">
+            <InputGroup>
+              <Form.Control
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+              />
+              <InputGroup.Text id="basic-addon1"><FaSearch /></InputGroup.Text>
+            </InputGroup>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
