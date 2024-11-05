c072b02088727183f31c3b684ce2e95652dfc380
Hadi <hadi.qaemi@gmail.com>
2024-11-05 10:10:04 +0100

    Implement new API

--- backend/interfaces/api.py
+++ backend/interfaces/api.py
@@ -1,9 +1,13 @@
-from flask import request
+from flask import request, jsonify
 from flask_restx import Api, Resource, fields
 from use_cases.paper_service import PaperService
+from http import HTTPStatus
+import math
 
 # Create the Flask-RESTX API object with version, title, and description.
-api = Api(version="1.0", title="TIB API", description="API for TIB data", doc='/api/swagger')
+api = Api(
+    version="1.0", title="TIB API", description="API for TIB data", doc="/api/swagger"
+)
 
 # Create an instance of the PaperService, which handles the business logic for paper data.
 paper_service = PaperService()
@@ -81,16 +85,64 @@ class PaperById(Resource):
     def get(self):
         entity_id = request.args.get("id")
         paper = paper_service.get_paper_by_id(entity_id)
-        # return vars(paper) if paper else api.abort(404, "Paper not found")
+        return paper
+
+# Define the endpoint for retrieving a paper by its entity ID.
+@api.route("/api/statement")
+@api.param("id", "The paper entity ID")
+class StatementById(Resource):
+    @api.doc("get_statement_by_id")
+    def get(self):
+        entity_id = request.args.get("id")
+        paper = paper_service.get_statement_by_id(entity_id)
         return paper
 
 
 # Define the endpoint for adding a new paper by its URL.
 @api.route("/api/add-paper")
 @api.param("url", "The paper entity URL")
-class PaperById(Resource):
+class AddPaper(Resource):
     @api.doc("add_paper_by_url")
     def post(self):
         url = request.args.get("url")
         paper = paper_service.extract_paper(url)
         return {"result": True}
+
+# Define the endpoint for searching.
+@api.route("/api/query-data")
+@api.param("url", "The paper entity URL")
+class get_data(Resource):
+    @api.doc("get_data")
+    def get(self):
+        author_ids = [int(id) for id in request.args.getlist("author_ids[]", type=int)]
+        concept_ids = [
+            int(id) for id in request.args.getlist("concept_ids[]", type=int)
+        ]
+        statement_filters = {
+            key.split("_", 1)[1]: value
+            for key, value in request.args.items()
+            if key.startswith("statement_")
+        }
+        article_filters = {
+            key.split("_", 1)[1]: value
+            for key, value in request.args.items()
+            if key.startswith("article_")
+        }
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 10, type=int)
+
+        response = paper_service.query_data(
+            author_ids, concept_ids, statement_filters, article_filters, page, per_page
+        )
+        status_code = HTTPStatus.OK if response['success'] else HTTPStatus.FAILED_DEPENDENCY
+        response = jsonify(
+            {
+                "content": response['result'],
+                "totalElements": response['total_count'],
+                "page": page,
+                "per_page": per_page,
+                "totalPages": math.ceil(response['total_count'] / per_page),
+            }
+        )
+        response.status_code = status_code
+        return response 
--- backend/use_cases/paper_service.py
+++ backend/use_cases/paper_service.py
@@ -2,7 +2,8 @@ from infrastructure.web_scraper import NodeExtractor
 from domain.entities import Paper, Contribution
 from bson.regex import Regex
 from urllib.parse import unquote
-from pymongo import MongoClient
+
+# from pymongo import MongoClient
 from infrastructure.repositories.data_repository import DataRepository
 import math
 
@@ -20,15 +21,6 @@ class PaperService:
         self.scraper = NodeExtractor()
 
     def get_all_papers(self):
-        """
-        Retrieves all papers from the database, excluding the "_id" field.
-        Returns:
-            list[Paper]: A list of Paper objects containing selected paper data.
-        """
-        # papers = self.db_client.find_all("papers", projection={
-        #     "contributions": 1, "title": 1, "author": 1, "dois": 1,
-        #     "entity": 1, "external": 1, "info": 1, "timeline": 1, "_id": 0
-        # })
         papers = self.db_client.get_all_items(
             "papers",
             projection={
@@ -84,43 +76,53 @@ class PaperService:
         }
 
     def get_paper_by_id(self, entity_id):
-        """
-        Retrieves a single paper by its entity ID from the database, excluding the "_id" field.
-        Args:
-            entity_id (str): The entity ID of the paper to retrieve.
-        Returns:
-            Paper: A Paper object if found, otherwise None.
-        """
-        # paper = self.db_client.find_one("papers", {"entity": entity_id}, projection={
-        #     "contributions": 1, "title": 1, "author": 1, "dois": 1,
-        #     "entity": 1, "external": 1, "info": 1, "timeline": 1, "_id": 0
-        # })
-        paper = self.db_client.find_one(
-            "papers",
-            {"title": entity_id},
-            projection={
-                "contributions": 1,
-                "title": 1,
-                "author": 1,
-                "dois": 1,
-                "entity": 1,
-                "external": 1,
-                "info": 1,
-                "timeline": 1,
-                "_id": 0,
-            },
-        )
-        # return Paper(**paper) if paper else None
-        return paper if paper else None
+        try:
+            article, statements = self.db_client.find_one("papers", entity_id)
+            return {
+                "success": True,
+                "result": {"article": article, "statements": statements},
+                "total_count": len(statements),
+            }
+
+        except Exception as e:
+            return {"success": False, "result": str(e), "total_count": 0}
+
+    def get_statement_by_id(self, entity_id):
+        try:
+            statement = self.db_client.find_one_statement("statements", entity_id)
+            return {
+                "success": True,
+                "result": {"statement": statement},
+                "total_count": len(statement),
+            }
+
+        except Exception as e:
+            return {"success": False, "result": str(e), "total_count": 0}
+
+    def query_data(
+        self,
+        author_ids,
+        concept_ids,
+        statement_filters,
+        article_filters,
+        page,
+        per_page,
+    ):
+        try:
+            data = self.db_client.query_search(
+                author_ids,
+                concept_ids,
+                statement_filters,
+                article_filters,
+                page,
+                per_page,
+            )
+            return {"success": True, "result": data, "total_count": len(data)}
+
+        except Exception as e:
+            return {"success": False, "result": str(e), "total_count": 0}
 
     def search_by_title(self, search_title):
-        """
-        Retrieves all single papers by their title from the database, excluding the "title" field.
-        Args:
-            title (str): The title of the papers to retrieve.
-        Returns:
-            Paper: A list of Paper objects if found, otherwise None.
-        """
         search_title = unquote(search_title)
         query = {
             "$or": [
@@ -147,38 +149,40 @@ class PaperService:
         return [Paper(**paper) for paper in papers]
 
     def extract_paper(self, url):
-        """
-        Extracts paper information from a given URL using the web scraper and stores it in the database.
-        Args:
-            url (str): The URL of the paper to extract information from.
-        Returns:
-            str: The ID of the inserted document in the database.
-        """
         self.scraper.set_url(url)
         title = self.scraper.title()
-        json_files, metadata = self.scraper.json_files()
-        contributions = []
-        for json in json_files:
-            contributions.append(self.scraper.load_json_from_url(json))
-        author = self.scraper.author()
-        dois, entity, external = self.scraper.DOI()
-        info = ""
-        if len(dois) > 0:
-            info = self.scraper.info()
-        elif len(metadata) > 0:
-            info = self.scraper.get_metadata(metadata)
+        json_files = self.scraper.all_json_files()
+        # print(json_files)
+        print(json_files["ro-crate-metadata.json"])
 
-        timeline = self.scraper.timeline()
+        ro_crate = self.scraper.load_json_from_url(json_files["ro-crate-metadata.json"])
+        info = self.db_client.add_article(ro_crate, json_files)
+        print(info)
+        return True
+        # contributions = []
+        # for json in json_files:
+        #     contributions.append(self.scraper.load_json_from_url(json))
+        # author = self.scraper.author()
+        # dois, entity, external = self.scraper.DOI()
+        # info = ""
+        # if len(dois) > 0:
+        #     info = self.scraper.info()
+        # elif len(metadata) > 0:
+        #     info = self.scraper.get_metadata(metadata)
+        # info = self.scraper.get_metadata(metadata)
+        # print(info)
 
-        data = {
-            "title": title,
-            "author": author,
-            "dois": dois,
-            "entity": entity,
-            "contributions": contributions,
-            "external": external,
-            "info": info,
-            "timeline": timeline,
-        }
-        inserted_id = self.db_client.add_item("papers", data)
-        return inserted_id
+        # timeline = self.scraper.timeline()
+
+        # data = {
+        #     "title": title,
+        #     "author": author,
+        #     "dois": dois,
+        #     "entity": entity,
+        #     "contributions": contributions,
+        #     "external": external,
+        #     "info": info,
+        #     "timeline": timeline,
+        # }
+        # inserted_id = self.db_client.add_item("papers", data)
+        # return inserted_id
